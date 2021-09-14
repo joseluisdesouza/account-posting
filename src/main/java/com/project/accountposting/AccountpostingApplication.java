76c1eb55ad6e003f7c748cbd8e16b9f4148cb4f9
josesouza <joseluisdesouzaa@gmail.com>
2021-09-14 15:08:21 -0300

    project start

---
+++ src/main/java/com/project/accountposting/AccountpostingApplication.java
@@ -0,0 +1,13 @@
+package com.project.accountposting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountpostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountpostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/accountposting/AccountpostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.accountposting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountpostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
