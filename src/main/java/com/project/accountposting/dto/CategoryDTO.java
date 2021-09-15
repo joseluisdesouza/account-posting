29a4a505c99807c1f5973fbd7b6c6de4ea7dbbba
josesouza <joseluisdesouzaa@gmail.com>
2021-09-15 14:08:30 -0300

    adjustment dto

--- src/main/java/com/project/accountposting/dto/CategoryDTO.java
+++ src/main/java/com/project/accountposting/dto/CategoryDTO.java
@@ -2,24 +2,14 @@ package com.project.accountposting.dto;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.Positive;
-
 @Data
-@Entity
 @With
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CategoryDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Positive
     private int activated;
     private String description;
 }
--- src/main/java/com/project/accountposting/dto/HistoryDTO.java
+++ src/main/java/com/project/accountposting/dto/HistoryDTO.java
@@ -2,22 +2,15 @@ package com.project.accountposting.dto;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.time.Instant;
 
 @Data
-@Entity
 @With
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class HistoryDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long titleId;
     private Long userId;
